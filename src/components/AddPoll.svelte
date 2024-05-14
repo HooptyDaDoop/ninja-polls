62946a6ee5cc49a0676a7f01cbf98adce13b31ff
Aronk112 <kvaszta.aron.24@outlook.com>
2024-05-14 18:24:24 +0200

    Adding polls planning

---
+++ src/components/AddPoll.svelte
@@ -0,0 +1,26 @@
+<!-- TODO | | Add question field -->
+<!-- TODO | | Create a single answer component (this is the correct answer) -->
+<!-- TODO | | Create a button to add a new answer component (for any invalid answers) -->
+<!-- TODO | | Create a button to submit the form -->
+<!-- TODO | | When the form is submitted, check send answer and question input fields for validation -->
+<!-- TODO | | If any fields are empty, create a component to let the user know. Don't submit form  -->
+<!-- TODO | | Otherwise, emit a custom event to submit the form data -->
+
+<!-- MARK: Logic
+-->
+<script>
+</script>
+
+
+<!-- MARK: Content
+-->
+<div>
+  <form on:submit|preventDefault={validateForm}></form>
+</div>
+
+
+<!-- MARK: Styles
+-->
+<style>
+
+</style>
